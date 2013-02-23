f93ab4b6f51ac0aea786d854cf9ed63f20d9d030
Łukasz Sędek <lukaszsedek@gmail.com>
2013-02-23 20:12:46 +0100

    Początek
    Maven + github. Proste okienko w javie

---
+++ mkoi/src/main/java/pl/edu/pw/tele/App.java
@@ -0,0 +1,28 @@
+package pl.edu.pw.tele;
+
+import frontend.MainFrame;
+
+/**
+ * Main Class
+ *
+ */
+public class App 
+{
+	/**
+	 * W¸asny obiekt.<br>
+	 * Referencja do superklasy JFrame
+	 */
+	static MainFrame myFrame = null;
+	
+    public static void main( String[] args )
+    {
+    	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+
+            	myFrame = new MainFrame();
+            	myFrame.setVisible(true);
+            }
+        });
+    	
+    }
+}
