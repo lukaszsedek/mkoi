125c4f4766c84067e7211bb2522a018bf250cdac
Łukasz Sędek <lukaszsedek@gmail.com>
2013-06-08 18:55:33 +0200

    last 8 cze

--- mkoi/src/main/java/pl/edu/pw/tele/enigma/Rotor.java
+++ mkoi/src/main/java/pl/edu/pw/tele/enigma/Rotor.java
@@ -1,5 +1,7 @@
 package pl.edu.pw.tele.enigma;
 
+import java.util.logging.Logger;
+
 /**
  * 
  * @author Lukasz
@@ -7,6 +9,7 @@ package pl.edu.pw.tele.enigma;
  */
 public class Rotor implements RotorInterface{
 
+	Logger log = Logger.getLogger(Rotor.class.getName());
 	char[] rotor;
 	char[] alphabet;
 	int position = 0;
@@ -14,6 +17,27 @@ public class Rotor implements RotorInterface{
 	public Rotor(){}
 	
 	public Rotor(String s){
+		log.info("Tworzenie rotora..." + s);
 		rotor = s.toCharArray();
+		Enigma enigma = Enigma.getInstance();
+		alphabet = enigma.getAlphabet();
+	}
+
+	@Override
+	public int getSize() {
+		return rotor.length;
+	}
+
+	public char[] getAlphabet() {
+		return alphabet;
+	}
+
+	public void setAlphabet(char[] alphabet) {
+		this.alphabet = alphabet;
+	}
+
+	@Override
+	public char[] getRotor() {
+		return rotor;
 	}
 }
